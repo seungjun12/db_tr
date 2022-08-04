7c198584de8e1feb7544e26489d90d73405925a1
seungjun12 <vndan110@naver.com>
2022-08-04 15:19:42 +0900

    dd

--- mysql/sql/busan.sql
+++ mysql/sql/busan.sql
@@ -6,5 +6,13 @@ select*from member where id ="cjftn" and pwd ="qwer"
 
 
 -- 회원가입
+SELECT
+	a.name
+    ,a.id
+    ,a.pwd
+    ,a.gender
+    ,a.email
+    ,a.emailCode
+    ,a.personalAgree -- 여기서부터 하면됨
 
 -- 쭉 다른 것들 생각해보자 ㅋ
