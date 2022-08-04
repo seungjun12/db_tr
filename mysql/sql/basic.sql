77fe3024459824ddb7fedde249e1df2c61c7d4bc
seungjun12 <vndan110@naver.com>
2022-08-04 17:26:45 +0900

    ㅇㅇ

--- mysql/sql/basic.sql
+++ mysql/sql/basic.sql
@@ -60,3 +60,5 @@ SELECT*FROM member;
 
 
 
+
+
--- mysql/sql/busan.sql
+++ mysql/sql/busan.sql
@@ -1,5 +1,14 @@
 USE Incheon;
 
+select
+	a.seq
+    ,a.ccgname
+    ,b.seq
+    ,b.ccname
+from ccg a
+left join cc b on b.seq = a.seq
+;
+
 -- 로그인
 select*from member where id ="cjftn" and pwd ="qwer"
 ;
@@ -13,6 +22,12 @@ SELECT
     ,a.gender
     ,a.email
     ,a.emailCode
-    ,a.personalAgree -- 여기서부터 하면됨
+    ,a.personalAgree 
+    ,b.member_seq
+    ,b.number
+from member a
+inner join memberPhone b on b.seq = a.seq
+;
+
 
 -- 쭉 다른 것들 생각해보자 ㅋ
