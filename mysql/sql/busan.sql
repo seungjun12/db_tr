1c8be857b061d2009b50e19d80c6c7f7f8999885
seungjun12 <vndan110@naver.com>
2022-08-10 14:03:18 +0900

    20220810 1403

--- mysql/sql/busan.sql
+++ mysql/sql/busan.sql
@@ -38,6 +38,19 @@ FROM games a
 WHERE 1=1
 	AND a.seq=1
 ;    
+
+-- 예매하기 (경기 선택) 
+SELECT
+	a.seq
+    ,a.time
+    ,a.dob
+FROM games a
+WHERE 1=1
+	AND a.seq=2
+;    
+ 
+
+
 -- 좌석 선택
 SELECT 
 	a.seq
@@ -58,6 +71,14 @@ FROM seatNumber a
 left join seatBlock b on b.seq = a.seatBlock_seq
 left join seatRow c on c.seq = a.seatRow_seq     
 ;
+
+-- 티켓예매(가격선택) : seat(좌석 위치,좌석등급,가격) seatBlock(블록), seatRow(열), seatNumber(좌석 번호), games(누가,어디서,경기날짜.경기시작시간), purchase(취소기한) 
+
+
+
+-- 티켓예매(배송 선택,주문자 확인) member(이름,생년월일,연락처,이메일), purchase(결제방법)(위에꺼)
+
+-- 티켓예매   
 
 
 -- 회원정보수정
